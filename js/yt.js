2302460b75911c47b06fdd9e0f369cfb1b92c666
Smu1zel <96768305+Smu1zel@users.noreply.github.com>
2023-12-31 14:33:18 -0500

    Fetch the latest video from YouTube automatically. (#4)
    * Refactor how the latest video is fetched. Change "Subscribe" to automatically prompt the user to subscribe.
    
    * Use YouTube NoCookie for parity, privacy, and to reduce the amount of errors logged in the console when uBlock Origin is installed.
    
    * Add a fallback message for when the video fails to play due to JavaScript being disabled.
    
    * Don't declare as paragraph.
    
    * Use the right URL.
    
    ---------
    
    Co-authored-by: Smu1zel

---
+++ js/yt.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Fetch the latest video on Bob's channel to later embed it. Uses the method found at https://stackoverflow.com/a/45342740.
+  var channelID = "UCAZzz2Dd9Xh6vTjNcxhqPZw";
+  var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+  $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL) + channelID, function(data) {
+    var link = data.items[0].link;
+    var id = link.substr(link.indexOf("=") + 1);
+    $("#youtube_video").attr("src", "https://www.youtube-nocookie.com/embed/" + id + "?rel=0&disablekb=1&cc_load_policy=1");
+  });
+});
